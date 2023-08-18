98c2381a15706061226ba4b042ae2ab705fc6042
Danil Simonov <simdanila@gmailcom>
2023-08-18 18:10:50 +0300

    удалил лишние импорты

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
